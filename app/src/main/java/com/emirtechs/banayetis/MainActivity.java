c6fd95cce1242e2e6bff6e71e4ae42856c9e115f
emiryucell <e.talhayucel@gmail.com>
2021-12-19 22:35:03 +0300

    The character has been moved.Improvements have been made.

--- app/src/main/java/com/emirtechs/banayetis/MainActivity.java
+++ app/src/main/java/com/emirtechs/banayetis/MainActivity.java
@@ -4,18 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView scoreText;
     TextView timeText;
     int score;
+    Button startButton;
 
     ImageView imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9;
 
     ImageView[] imageArray;
+
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +40,53 @@ public class MainActivity extends AppCompatActivity {
         imageView8 = findViewById(R.id.imageView8);
         imageView9 = findViewById(R.id.imageView9);
 
+        startButton = findViewById(R.id.startButton);
+
         imageArray = new ImageView[]{imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9};
 
-hideImages();
 
         timeText = findViewById(R.id.timeText);
         scoreText = findViewById(R.id.scoreText);
         score = 0;
+        scoreText.setText("Score: " + score);
 
-        new CountDownTimer(30000, 1000) {
+
+    }
+
+    public void increaseScore(View view) {
+        score++;
+        scoreText.setText("Score: " + score);
+    }
+
+    public void hideImages() {
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                for (ImageView image : imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageArray[i].setVisibility(View.VISIBLE);
+
+                handler.postDelayed(this, 500);
+            }
+        };
+
+        handler.post(runnable);
+        startButton.setEnabled(false);
+
+    }
+
+    public void startGame(View view) {
+        score = 0;
+        scoreText.setText("Score: 0");
+        hideImages();
+
+        new CountDownTimer(3000, 1000) {
 
             @Override
             public void onTick(long l) {
@@ -49,19 +95,14 @@ hideImages();
 
             @Override
             public void onFinish() {
+                startButton.setEnabled(true);
 
+                timeText.setText("Time Off");
+                handler.removeCallbacks(runnable);
+                for (ImageView image : imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
             }
         }.start();
-
-    }
-
-    public void increaseScore(View view) {
-        score++;
-        scoreText.setText("Score: " + score);
-    }
-    public void hideImages(){
-        for(ImageView image: imageArray){
-            image.setVisibility(View.INVISIBLE);
-        }
     }
 }
